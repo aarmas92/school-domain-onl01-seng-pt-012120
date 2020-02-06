f42287b6a20676f5d31eff7d50d7f50a29948f2a
Alvaro Armas <aarmas92@outlook.com>
2020-02-06 23:51:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,7 +15,10 @@ class School
   def add_student(name, grade)
     @student_name = name
     @grade = grade
-   
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
   end
 
 
